e811ce2c2f2b5aaed4df5e0608513d75d48f3b0e
Hugo Gualdron <hagc2812@gmail.com>
2016-01-12 22:02:35 -0200

    Adding periodic notifications

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-    var options = {
-        type : "image",
-        title: "Random Words",
-        message: "Word of the " + Date.now() + "",
-        iconUrl: "icon.png",
-        imageUrl: "image.png"
+    var alarminfo = {
+        when: Date.now(),
+	   periodInMinutes: 1
     };
-    chrome.notifications.create("id", options);
+
+    chrome.alarms.onAlarm.addListener(function( alarm ){
+	   var options = {
+          type : "image",
+          title: "Random Words",
+          message: "Word of the " + Date.now() + "",
+          iconUrl: "icon.png",
+          imageUrl: "image.png"
+        };
+        chrome.notifications.create("id", options);
+    });
+    chrome.alarms.create("alarm1", alarminfo);
+
 });
+
