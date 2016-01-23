ab3f12bd3faf1c995a787de6bdfe961e66e2631c
Antoonio Canabrava Fraideinberze <antoniocfrai@gmail.com>
2016-01-23 19:01:29 -0200

    Switched to Notification API. It works from time to time, but it should be nice to change everything to work without chrome dependencies. Plus need to check a way to the the url (cross referencing) from other websites so we can parse them.

--- main.js
+++ main.js
@@ -9,13 +9,24 @@ chrome.app.runtime.onLaunched.addListener(function() {
           type : "image",
           title: "Random Words",
           message: "Word of the " + Date.now() + "",
-          iconUrl: "icon.png",
-          imageUrl: "image.png"
+          iconUrl: "http://static.sfdict.com/sizedimage/sizedimage?width=455&height=455&url=http%3A%2F%2Fstatic.sfdict.com%2Fstatic%2Fwotd%2Ftiles%2F20160123_selenology.png",
+          imageUrl: "http://static.sfdict.com/sizedimage/sizedimage?width=455&height=455&url=http%3A%2F%2Fstatic.sfdict.com%2Fstatic%2Fwotd%2Ftiles%2F20160123_selenology.png"
         };
-        chrome.notifications.create("id" + Date.now(), options);
+        //chrome.notifications.create("id" + Date.now(), options);
+
+        var n = new Notification("Random Words", getOptions());
+
 	console.log("Notification " + Date.now());
     });
     chrome.alarms.create("alarm1", alarminfo);
 
 });
 
+
+function getOptions() {
+  // var html = GET THE URL
+  var element = .getElementsByClassName('definition-header');
+  console.log(element.innerHTML);
+
+  return {tile: "test.", body: "ticatica"};
+}
